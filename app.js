f2c7633ae5a47bd4f38a1ffb0c910a57d4f81047
Aleksandra Ksiazek <aleksandra.ksiazek@dazn.com>
2022-12-27 11:04:46 +0100

    BE-challenge: Implement CR comments

--- app.js
+++ app.js
@@ -4,16 +4,6 @@ import { router as stream } from './stream.js';
 const app = express();
 const port = 3000;
 
-// app.post('/reservation/{userId}', (req, res) => {
-//   res.send()
-//   console.log('reservation has been MADE');
-// })
-
-// app.post('/removeReservation/{userId}', (req, res) => {
-//   res.send()
-//   console.log('reservation has been REMOVED');
-// })
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- test/integration/streamCounter.test.js
+++ test/integration/streamCounter.test.js
@@ -1,51 +1,55 @@
 import got from 'got';
-import random from 'random';
+import { v4 as uuidv4 } from 'uuid';
+import { ENDPOINT_URL } from '../constants';
 
 const testUserId = 'test_user_1987';
-const randomTestUserId = random.int(0, 1000000);
+const randomTestUserId = uuidv4();
+const maxStreams = 3;
 
 describe('Test streamService endpoints', () => {
+    
     afterEach(async () => {
-        got.post(`http://localhost:3000/stream/removeReservation/${testUserId}`);
-        got.post(`http://localhost:3000/stream/removeReservation/${testUserId}`);
-        got.post(`http://localhost:3000/stream/removeReservation/${testUserId}`);
+        for(let i = 0; i < maxStreams; i++) {
+            await got.post(`${ENDPOINT_URL}/removeReservation/${testUserId}`);
+        }
     });
 
     it('First time request for a stream should return 200 response', async () => {
-        const response = await got.post(`http://localhost:3000/stream/reservation/${testUserId}`);
+        const response = await got.post(`${ENDPOINT_URL}/reservation/${testUserId}`);
         expect(response.statusCode).toBe(200);
     });
 
     it('Exceeding the number of streams should result in 400 being returned', async () => {
-        await got.post(`http://localhost:3000/stream/reservation/${testUserId}`);
-        await got.post(`http://localhost:3000/stream/reservation/${testUserId}`);
-        await got.post(`http://localhost:3000/stream/reservation/${testUserId}`);
+        for (let i = 0; i < maxStreams; i++) {
+            await got.post(`${ENDPOINT_URL}/reservation/${testUserId}`);
+        }
         let statusCode;
         try {
-            await got.post(`http://localhost:3000/stream/reservation/${testUserId}`);
+            await got.post(`${ENDPOINT_URL}/reservation/${testUserId}`);
         } catch(e) {
             statusCode = e.response.statusCode;
         }
         expect(statusCode).toBe(400);
     });
 
-    it('Requesting stream 3 times and then one request to remove one stream should result in 200 being returned', async () => {
-        await got.post(`http://localhost:3000/stream/reservation/${testUserId}`);
-        await got.post(`http://localhost:3000/stream/reservation/${testUserId}`);
-        await got.post(`http://localhost:3000/stream/reservation/${testUserId}`);
-        const response = await got.post(`http://localhost:3000/stream/removeReservation/${testUserId}`);
+    it('Requesting a stream 3 times, removing one reservation and then requesting a stream again should result in 200 being returned', async () => {
+        for (let i = 0; i < maxStreams; i++) {
+            await got.post(`${ENDPOINT_URL}/reservation/${testUserId}`);
+        }
+        await got.post(`${ENDPOINT_URL}/removeReservation/${testUserId}`);
+        const response = await got.post(`${ENDPOINT_URL}/reservation/${testUserId}`);
         expect(response.statusCode).toBe(200);
     });
 
-    it('One request for a stream, then a request to remove a stream, and then again a request to remove a stream should result in 200 being returned', async () => {
-        await got.post(`http://localhost:3000/stream/reservation/${testUserId}`);
-        await got.post(`http://localhost:3000/stream/removeReservation/${testUserId}`);
-        const response = await got.post(`http://localhost:3000/stream/removeReservation/${testUserId}`);
+    it('Removing a reservation for a user with 0 current reservations should result in a 200 response', async () => {
+        await got.post(`${ENDPOINT_URL}/reservation/${testUserId}`);
+        await got.post(`${ENDPOINT_URL}/removeReservation/${testUserId}`);
+        const response = await got.post(`${ENDPOINT_URL}/removeReservation/${testUserId}`);
         expect(response.statusCode).toBe(200);
     });
 
     it('One time request to remove a stream with userId which has not been in database should result in 200 being returned', async () => {
-        const response = await got.post(`http://localhost:3000/stream/removeReservation/${randomTestUserId}`);
+        const response = await got.post(`${ENDPOINT_URL}/removeReservation/${randomTestUserId}`);
         expect(response.statusCode).toBe(200);
     });
 });
