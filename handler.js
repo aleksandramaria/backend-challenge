c50c040b3c97bf8cb0cfe97f5aebdc92bf460a43
Aleksandra Ksiazek <aleksandra.ksiazek@dazn.com>
2023-01-03 16:12:15 +0100

    BE-challenge: Add serverless

---
+++ handler.js
@@ -0,0 +1,19 @@
+'use strict';
+
+export const hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+        message: 'v1.0!'
+      }),
+  };
+};
+
+export const reservation = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+        message: 'here is your reservation'
+      }),
+  };
+};
