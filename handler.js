30f419419b08649b6953dcbb5b0aeff300962731
Aleksandra Ksiazek <aleksandra.ksiazek@dazn.com>
2023-01-10 17:38:05 +0100

    BE-challenge: Switching to serverless and lambdas

--- handler.js
+++ handler.js
@@ -1,17 +1,55 @@
-export const hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-        message: 'v1.0!'
-      }),
-  };
+class ServerError extends Error {
+    constructor(statusCode, message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+const counters = new Map();
+const increaseCounter = (userId) => {
+    if (counters.has(userId)) {
+        const value = counters.get(userId);
+        if (value === 3) {
+            throw new ServerError(400, 'You have reached the maximum number of streams allowed');
+        }
+        counters.set(userId, value + 1);
+    } else {
+        counters.set(userId, 1);
+    }
 };
 
-export const reservation = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-        message: 'here is your reservation'
-      }),
-  };
+const decreaseCounter = (userId) => {
+    if (counters.has(userId)) {
+        const value = counters.get(userId);
+        if (value !== 0) {
+            counters.set(userId, value - 1);
+        }
+    }
+};
+
+export const reservation = async (event, context) => {
+    try {
+        increaseCounter(event.pathParameters.userId);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'OK',
+            }),
+        };
+    } catch (err) {
+        return {
+            statusCode: err.statusCode,
+            body: JSON.stringify(err),
+        };
+    }
+};
+
+export const removeReservation = async (event, context) => {
+    decreaseCounter(event.pathParameters.userId);
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'OK',
+        }),
+    };
 };
